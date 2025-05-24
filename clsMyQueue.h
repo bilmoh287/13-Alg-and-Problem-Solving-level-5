98f1ad7bdb36cb083881d02074c99341978eb87b
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-05-24 10:18:08 +0300

    added Extension 1-7 on my clsMyQueue Lib

--- clsMyQueue.h
+++ clsMyQueue.h
@@ -52,7 +52,40 @@ public:
 		return _MyList.GetItem(Size() - 1);
 	}
 
+	T GetItem(int index)
+	{
+		return _MyList.GetItem(index);
+	}
 
+	void Reverse()
+	{
+		_MyList.Reverese();
+	}
+
+	void UpdateItem(int index, T value)
+	{
+		_MyList.UpdateItem(index, value);
+	}
+
+	void InsertAfter(int index, T value)
+	{
+		_MyList.InsertAfter(index, value);
+	}
+
+	void InsertAtFront(T value)
+	{
+		_MyList.InsertAtBeginning(value);
+	}
+
+	void InsertAtBack(T value)
+	{
+		_MyList.InsertAtEnd(value);
+	}
+
+	void Clear()
+	{
+		_MyList.Clear();
+	}
 
 };
 
--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include "clsDblLinkedList.h"
 #include "clsMyQueue.h"
 
 using namespace std;
 
+int main()
+{
 
-int main() {
     clsMyQueue <int> MyQueue;
 
     MyQueue.push(10);
@@ -14,7 +14,8 @@ int main() {
     MyQueue.push(40);
     MyQueue.push(50);
 
-    cout << "\nQueue:\n";
+
+    cout << "\nQueue: \n";
     MyQueue.Print();
 
     cout << "\nQueue Size: " << MyQueue.Size();
@@ -23,11 +24,46 @@ int main() {
 
     MyQueue.pop();
 
-    cout << "\n\nQueue after pop:\n";
+    cout << "\n\nQueue after pop() : \n";
     MyQueue.Print();
-    cout << "\n" << MyQueue.IsEmpty();
+
+    //Extension #1
+    cout << "\n\n Item(2) : " << MyQueue.GetItem(2);
+
+    //Extension #2
+    MyQueue.Reverse();
+    cout << "\n\nQueue after reverse() : \n";
+    MyQueue.Print();
+
+    //Extension #3
+    MyQueue.UpdateItem(2, 600);
+    cout << "\n\nQueue after updating Item(2) to 600 : \n";
+    MyQueue.Print();
+
+    //Extension #4
+    MyQueue.InsertAfter(2, 800);
+    cout << "\n\nQueue after Inserting 800 after Item(2) : \n";
+    MyQueue.Print();
+
+
+    //Extension #5
+    MyQueue.InsertAtFront(1000);
+    cout << "\n\nQueue after Inserting 1000 at front: \n";
+    MyQueue.Print();
+
+    //Extension #6
+    MyQueue.InsertAtBack(2000);
+    cout << "\n\nQueue after Inserting 2000 at back: \n";
+    MyQueue.Print();
+
+    //Extension #7
+    MyQueue.Clear();
+    cout << "\n\nQueue after Clear(): \n";
+    MyQueue.Print();
+
+
+
 
     system("pause>0");
 
-    return 0;
 }
